049e5140014bfcfab7003bdc5aaf19c72a2626e8
beebrown <bruno.grosdesirs@outlook.fr>
2023-11-04 14:22:01 +0100

    maj

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,7 +86,6 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    console.log(bill)
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
@@ -146,7 +145,7 @@ export default class {
       this.counter++
     }
 
-    bills.forEach(bill => {
+    filteredBills(bills, getStatus(this.index)).forEach(bill => {
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
