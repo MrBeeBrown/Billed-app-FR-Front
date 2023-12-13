f9a6bb69816d4a245f52f9a65757dc21c5f195f2
beebrown <bruno.grosdesirs@outlook.fr>
2023-12-13 09:59:19 +0100

    maj

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -17,18 +17,17 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
+}
 
 
-//[BUG REPORT - Bills] Trier les NDF par date
 //Trier les notes de frais par date
 const rows = (data) => {
-  return (data && data.length) ? data.sort((a, b) => new Date(b.date) - new Date(a.date)).map(bill => row(bill)).join("") : "" 
+  return (data && data.length) ? data.sort((a, b) => new Date(b.date) - new Date(a.date)).map(bill => row(bill)).join("") : ""
 }
 
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -51,7 +50,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
